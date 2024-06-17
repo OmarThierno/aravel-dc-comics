116c0e4ca8f7b16996aa80872b9a47a3ffb3ecda
OmarThierno <thiernomarndiaye76@gmail.com>
2024-06-17 15:35:04 +0200

    feat: add delete crud function

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,7 +6,7 @@
         <h1>Comics</h1>
         <a href="{{route('comics.create')}}" class="btn btn-primary">Create</a>
       </div>
-      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 row-cols-xl-6 g-3 py-4">
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 row-cols-xl-6 g-5 py-4">
         @foreach ($comics as $comic)
         <div class="col">
           <div class="card h-100 pb-5">
@@ -16,8 +16,13 @@
               <p class="card-text">{{$comic->series}}</p>
               <p class="card-text">{{$comic->type}}</p>
               <div class="ms_btn d-flex">
-                <a href="{{route('comics.show', ['comic' => $comic->id])}}" class="btn btn-primary me-2">Details</a>
-                <a href="{{route('comics.edit', ['comic' => $comic->id])}}" class="btn btn-warning">Modify</a>
+                <a href="{{route('comics.show', ['comic' => $comic->id])}}" class="btn btn-primary ">Details</a>
+                <a href="{{route('comics.edit', ['comic' => $comic->id])}}" class="btn btn-warning ">Modify</a>
+                <form action="{{route('comics.destroy', ['comic'=> $comic->id])}}" method="POST">
+                  @method('DELETE')
+                  @csrf
+                  <button class="btn btn-danger">Delete</button>
+                </form>
               </div>
             </div>
           </div>
